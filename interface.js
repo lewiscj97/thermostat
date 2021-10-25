3b1375bd9597ef7604f804930d6688bf1afba442
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-25 17:00:39 +0100

    refactor(*): clean up interface

--- interface.js
+++ interface.js
@@ -1,34 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
   const updateTemperature = () => {
     document.querySelector('#current-temperature').innerText = thermostat.temperature;
-  }
+  };
 
   const updatePowerSavingMode = () => {
     let powerSaving = document.querySelector('#power-saving-mode-status');
     powerSaving.innerHTML = thermostat.powerSavingMode ? 'On' : 'Off';
+  };
+
+  const updateCurrentEnergyUsage = () => {
+    let energyUsage = document.querySelector('#current-energy-usage-status');
+    energyUsage.innerHTML = thermostat.currentEnergyUsage();
+  };
+
+  const updateThermostat = () => {
+    updateTemperature();
+    updatePowerSavingMode();
+    updateCurrentEnergyUsage();
   }
 
   const thermostat = new Thermostat();
-  updateTemperature();
-  updatePowerSavingMode();
+  updateThermostat();
 
   document.querySelector('#temperature-up').addEventListener('click', () => {
     thermostat.up();
-    updateTemperature();
+    updateThermostat();
   });
 
   document.querySelector('#temperature-down').addEventListener('click', () => {
     thermostat.down();
-    updateTemperature();
+    updateThermostat();
   });
 
   document.querySelector('#temperature-reset').addEventListener('click', () => {
     thermostat.reset();
-    updateTemperature();
+    updateThermostat();
   });
 
   document.querySelector('#power-saving-mode-toggle').addEventListener('click', () => {
     thermostat.togglePowerSavingMode();
-    updatePowerSavingMode();
+    updateThermostat();
   });
 });
